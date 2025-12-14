64bc97c3e9e2b6c5f8ec37708e7d69c1198300b1
ShashikantShirsath <shashikantshirsath08@gmail.com>
2025-12-14 19:30:35 +0530

    feat: implemented registration functionality, applied validations, handled errors and used toast message for better user experience

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./auth/AuthContext";
 import Login from "./pages/Login";
 import Sweets from "./pages/Sweets";
+import Register from "./pages/Register";
+
+import ProtectedRoute from "./auth/ProtectedRoute";
+import PublicRoute from "./auth/PublicRoute";
+
 
 function AppContent() {
   const { token } = useAuth();
@@ -10,9 +16,40 @@ function AppContent() {
 function App() {
 
   return (
-    <AuthProvider>
-      <AppContent />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Sweets />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/auth/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../auth/AuthContext";
+import type { JSX } from "react";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: JSX.Element;
+}) {
+  const { token } = useAuth();
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
